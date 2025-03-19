7d9ff579f3b62b7d99e223209195d031846bd1b1
ItsFuchsia <havardfarden@gmail.com>
2025-03-19 22:48:59 +0100

    initliazed repo

---
+++ extractingstuff.py
@@ -0,0 +1,16 @@
+currentarticle = ""
+
+with open("regs.txt", "r") as f:
+    with open("guidelines.csv", "r+") as g:
+        all = f.readlines()
+        storedguidelines = g.readlines()
+        for reg in all:
+            if reg.startswith("Article"):
+                currentarticle = reg.split(":")[0]
+            if (reg.split(")")[0]).endswith("+") and reg not in storedguidelines:
+                justreg = reg.split(")")
+                regnum = justreg.pop(0).rstrip()
+                g.write(regnum+ "\t"+ "".join(justreg).rstrip() + "\t Article " + reg[0] + "\n")
+
+
+print("completed")
